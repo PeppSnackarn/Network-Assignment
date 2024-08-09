4d93dae6713e8d4bb7bfab7ebccb35cee6e2129f
RainyFluff <nikodemius.ivarsson@elev.ga.lbs.se>
2024-08-09 14:23:43 +0200

    Movement replicated correctly across clients
    Next step is to fix the bullets spawning correctly.

--- Networking assignment/Assets/Scripts/Gun.cs
+++ Networking assignment/Assets/Scripts/Gun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -10,7 +11,16 @@ public class Gun : MonoBehaviour
 
    public void Fire()
    {
-      GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-      bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+      SpawnBulletRPC();
+      //GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+      //bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+   }
+
+   [Rpc(SendTo.Server)]
+   private void SpawnBulletRPC()
+   {
+      NetworkObject ob = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation).GetComponent<NetworkObject>();
+      ob.Spawn();
+      ob.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
    }
 }
